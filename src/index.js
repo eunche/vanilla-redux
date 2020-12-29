0d949c7834c0867266b6937a462b14ed0543abdb
eunchae619 <eunchae619@naver.com>
2020-12-29 22:00:39 +0900

    redux로 counter +1/-1기능 action을 통해서 구현

--- src/index.js
+++ src/index.js
@@ -5,13 +5,22 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 
-// store를 만들기위해 필요한것,
-// data를 찾고/수정하는 함수가 reducer임
-const countModifier = (count = 0) => {
-  return count
+const countModifier = (count = 0, action) => {
+  console.log(count, action)
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-// data를 저장하는 곳
 const countStore = createStore(countModifier)
 
-console.log(countStore)
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "ADD" })
+countStore.dispatch({ type: "MINUS" })
