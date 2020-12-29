a2a5f6a5b9a882175e7c5af5139622e501491b73
eunchae619 <eunchae619@naver.com>
2020-12-29 21:36:52 +0900

    redux로 store/reducer 만들고 state=0 값 초기화

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+// store를 만들기위해 필요한것,
+// data를 찾고/수정하는 함수가 reducer임
+const countModifier = (state = 0) => {
+  return state
 };
 
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
+// data를 저장하는 곳
+const countStore = createStore(countModifier)
 
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
-
-
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore)
