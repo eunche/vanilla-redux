deb00656c046ee2411ca4fbd8205e54134a90e80
eunchae619 <eunchae619@naver.com>
2020-12-30 15:46:57 +0900

    store의 메소드인 subscribe를 이용해서 store의 데이터가 변할때마다 onChange 함수를 실행시켜 span값에 넣음

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const number = document.querySelector("span");
 
 
 const countModifier = (count = 0, action) => {
-  console.log(count, action)
   if (action.type === "ADD") {
     return count + 1;
   } else if (action.type === "MINUS") {
@@ -18,9 +17,20 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier)
 
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "MINUS" })
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" })
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" })
+};
+
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
