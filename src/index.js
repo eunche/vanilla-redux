3eddd2222443619dfd5b3ba56853d7124ff80907
eunchae619 <eunchae619@naver.com>
2020-12-31 15:43:25 +0900

    reducer에서 DELETE_TODO action 구현

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const reducer = (state = [], action) => {
         case ADD_TODO:
             return [...state, { text: action.text, id: Date.now() }];
         case DELETE_TODO:
-            return [];
+            return state.filter(toDo => toDo.id !== action.id)
         default:
             return state;
     };
@@ -46,7 +46,7 @@ const dispatchAddTodo = (text) => {
 }
 
 const dispatchDeleteToDo = e => {
-    const id = e.target.parentNode.id;
+    const id = parseInt(e.target.parentNode.id);
     store.dispatch(deleteToDo(id));
 }
 
