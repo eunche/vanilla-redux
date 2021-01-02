7f07fdaac5ba25321e732a98a2d9a5924e5d77ca
eunchae619 <eunchae619@naver.com>
2021-01-02 16:35:03 +0900

    Home.js에서 connect의 mapDispatchToProps 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home(props) {
-    console.log(props)
+function Home({ toDos, addToDo }) {
     const [text, setText] = useState("");
     function onChange(e) {
         setText(e.target.value);
     }
     function onSubmit(e) {
         e.preventDefault();
-        setText("")
+        addToDo(text);
+        setText("");
     }
     return (
         <>
@@ -18,7 +19,7 @@ function Home(props) {
                 <input type="text" value={text} onChange={onChange} />
                 <button>Add</button>
             </form>
-            <ul></ul>
+            <ul>{JSON.stringify(toDos)}</ul>
         </>
     )
 }
@@ -27,4 +28,10 @@ function mapStateToProps(state) {
     return { toDos: state }
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+    return {
+        addToDo: (text) => dispatch(actionCreators.addToDo(text))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
