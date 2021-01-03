32ace210bc111d03c5ad56442c6e49391803391a
eunchae619 <eunchae619@naver.com>
2021-01-03 19:21:26 +0900

    redux toolkit 이용하여 코드개선 -action,reducer부분 수정

--- src/store.js
+++ src/store.js
@@ -1,29 +1,37 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-const addToDo = (text) => {
-    return {
-        type: ADD,
-        text
-    };
-};
+// const addToDo = (text) => {
+//     return {
+//         type: ADD,
+//         text
+//     };
+// };
 
-const deleteToDo = (id) => {
-    return {
-        type: DELETE,
-        id
-    };
-};
+// const deleteToDo = (id) => {
+//     return {
+//         type: DELETE,
+//         id
+//     };
+// };
+// 대체 코드
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
+console.log(addToDo(), deleteToDo())
+
+
+
 
 const reducer = (state = [], action) => {
     state = (localStorage.getItem('state') !== null) ? JSON.parse(localStorage.getItem('state')) : state;
     switch (action.type) {
-        case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
+        case addToDo.type:
+            return [{ text: action.payload, id: Date.now() }, ...state];
+        case deleteToDo.type:
+            return state.filter(toDo => toDo.id !== action.payload);
         default:
             return state;
     }
